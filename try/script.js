9150d26d07c3cf3e55a87b0762cc8610aeee23b1
RiaVenice <riatcruz.v1@gmail.com>
2025-03-28 01:04:38 +0800

    try

---
+++ try/script.js
@@ -0,0 +1,32 @@
+//scroller for the item boxes
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
+
+//navbar 
+/*window.addEventListener("scroll", function () {
+    const options = document.querySelector(".options");
+    const homepage = document.querySelector(".homepage");
+
+    if (window.scrollY >= homepage.offsetHeight) {
+        options.classList.add("sticky");
+    } else {
+        options.classList.remove("sticky");
+    }
+});*/
+
+
+
